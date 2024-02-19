afafb900d4c28f7b986114cde4c7bbb70f7ddb6e
Sanketr123 <rautsanket33@gmail.com>
2024-02-19 22:29:57 +0530

    file upload

--- src/main/java/pageObjects/PIMDetails_object.java
+++ src/main/java/pageObjects/PIMDetails_object.java
@@ -10,6 +10,15 @@ public class PIMDetails_object {
 	@FindBy(xpath = "(//div[@class='oxd-select-text-input' ])[1]")  //  oxd-select-dropdown --position-bottom
 	WebElement editbutton;
 	
+	@FindBy(xpath = "(//i[@class='oxd-icon bi-calendar oxd-date-input-icon'])[2]")  //  oxd-select-dropdown --position-bottom
+	WebElement dob;
+	
+	@FindBy(xpath = "(//span[@class='oxd-radio-input oxd-radio-input--active --label-right oxd-radio-input'])[1]")  //  oxd-select-dropdown --position-bottom
+	WebElement gender;
+	
+	@FindBy(xpath = "//button[@class='oxd-button oxd-button--medium oxd-button--text']")  //  oxd-select-dropdown --position-bottom
+	WebElement addattachment;
+	
 
 	public PIMDetails_object(WebDriver driver) {
 
@@ -25,7 +34,28 @@ public class PIMDetails_object {
 
 	}
 	
+	public void selectdate() throws InterruptedException {
+
+		editbutton.sendKeys("J");
+		Thread.sleep(4000);
+		editbutton.click();
+		
+
+	}
 	
+	public void selectGender() throws InterruptedException {
+
+		gender.click();
+		
+
+	}
+	
+	public void addFile() throws InterruptedException {
+
+		
+		
+
+	}
 	
 
 }
