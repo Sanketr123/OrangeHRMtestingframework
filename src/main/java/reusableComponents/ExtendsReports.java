b07f905b75f89e1be4be487f895ca0cf3ab794fd
Sanketr123 <rautsanket33@gmail.com>
2024-02-28 23:35:22 +0530

    working 28-02

--- src/main/java/reusableComponents/ExtendsReports.java
+++ src/main/java/reusableComponents/ExtendsReports.java
@@ -1,11 +1,38 @@
 package reusableComponents;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class ExtendsReports {
 	
 	
 	public void ExtenReport() {
 		
-		ExtendReports extend =new ExtendReports();
+		SimpleDateFormat format = new SimpleDateFormat("dd-mm-yyyy");
+		
+		Date date = new Date();
+		
+		String actualdate = format.format(date);		
+		
+		String reportPath = System.getProperty("user.dir")+ "\\target\\ExecutonReport"+actualdate+".html";
+		ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
+		
+		ExtentReports extend =new ExtentReports();
+		
+		extend.attachReporter(sparkReporter);
+		
+		sparkReporter.config().setTheme(Theme.DARK);
+		
+		sparkReporter.config().setDocumentTitle("Orange HRM POM Framework practice");
+		
+		return;
+		
+		
+		
 	}
 
 }
